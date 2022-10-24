6dedff94ed0c9f156f453bbf72d755ee17690b6f
Lucas Minamihara <Lucas.hmds@outlook.com>
2022-10-24 19:08:16 -0300

    Movimento Nave

---
+++ readme.md
@@ -0,0 +1,3 @@
+Fazer os inimigos aparecerem aleatoriamente pela tela do jogo
+Adicionar vida tanto nos inimigos como na nave
+Definir um tempo de spawn dos inimigos
--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,45 @@
 document.addEventListener("DOMContentLoaded", startGame);
 
 function startGame() {
+  const telaDoJogo = document.querySelector("#telaDoJogo");
   const nave = document.querySelector(".nave");
   const tiro = document.querySelector(".tiro");
+  const inimigos = `<div class="inimigos"></div>`;
   let positionTopBottom = 0;
   let positionLeftRight = 0;
   window.addEventListener("keydown", (e) => {
+    console.log(e.key);
     if (e.key == " ") {
       tiro.style.display = "block";
+      setTimeout(() => {
+        tiro.style.display = "none";
+      }, 100);
+    }
+    if (e.key == "Control") {
+      nave.classList.toggle("virarNave");
     }
     if (e.key == "ArrowDown") {
       positionTopBottom += 20;
       nave.style.top = positionTopBottom + "px";
-      console.log(positionTopBottom);
     }
     if (e.key == "ArrowUp") {
       positionTopBottom -= 20;
-      nave.style.bottom = positionTopBottom + "px";
-      console.log(positionTopBottom);
+      nave.style.top = positionTopBottom + "px";
     }
     if (e.key == "ArrowRight") {
       positionLeftRight += 20;
-      nave.style.right = positionLeftRight + "px";
-      console.log(positionLeftRight);
+      nave.style.left = positionLeftRight + "px";
     }
     if (e.key == "ArrowLeft") {
       positionLeftRight -= 20;
       nave.style.left = positionLeftRight + "px";
-      console.log(positionLeftRight);
     }
   });
+  // Spawn de inimigos
+  setInterval(() => {
+    
+    let topAleatorio = Math.random() * 1000;
+    let leftAleatorio = Math.random() * 1000;
+    telaDoJogo.insertAdjacentElement(inimigos);
+  }, 20000);
 }
