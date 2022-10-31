744f508036b0b63120128f202d55488d24ceeb20
Lucas Hideki Minamihara dos Santos <lucas@Jobcenter.net>
2022-10-31 10:14:29 -0300

    Posição do Inimigo e do disparo

--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 Fazer os inimigos aparecerem aleatoriamente pela tela do jogo
 Adicionar vida tanto nos inimigos como na nave
 Definir um tempo de spawn dos inimigos
+# Resolver conflito refente a posição do disparo e a posição do inimigo! 
+
+# Se o inimigo for atingido, teremos que diminuir a sua vida e ao atingí-lo 3 vezes consecutivas, teremos que eliminá-lo do jogo!  
--- scripts/script.js
+++ scripts/script.js
@@ -6,14 +6,18 @@ function startGame() {
   const nave = document.querySelector(".nave");
   const tiro = document.querySelector(".tiro");
   const inimigo = document.querySelector(".inimigo");
+  let posicaoInimigoX;
+  let posicaoInimigoY;
+  let posicaoTiroX;
+  let posicaoTiroY;
   let positionTopBottom = 0;
   let positionLeftRight = 0;
-  window.addEventListener("keydown", (e) => {
+  window.addEventListener("keydown", async (e) => {
     if (e.key == " ") {
       tiro.style.display = "block";
       setTimeout(() => {
         tiro.style.display = "none";
-      }, 100);
+      }, 200);
     }
     if (e.key == "Control") {
       nave.classList.toggle("virarNave");
@@ -35,14 +39,36 @@ function startGame() {
       nave.style.left = positionLeftRight + "px";
     }
   });
+  try {
+    setInterval(() => {
+      let topAleatorio = Math.random() * 1000;
+      let leftAleatorio = Math.random() * 1000;
+      inimigo.style.left = leftAleatorio + "px";
+      inimigo.style.top = topAleatorio + "px";
+    }, 30);
 
+    setInterval(() => {
+      posicaoInimigoX = inimigo.getBoundingClientRect().x;
+      posicaoInimigoY = inimigo.getBoundingClientRect().y;
+    }, 2000);
 
-  // setInterval(() => {
-  //   let topAleatorio = Math.random() * 1000;
-  //   let leftAleatorio = Math.random() * 1000;
-  //   inimigo.style.left = leftAleatorio + "px";
-  //   inimigo.style.top = topAleatorio + "px";
-  // }, 10);
+    setInterval(() => {
+      posicaoTiroX = tiro.getBoundingClientRect().x;
+      posicaoTiroY = tiro.getBoundingClientRect().y;
+    }, 100);
+
+    setInterval(() => {
+      if (
+        posicaoInimigoX === posicaoTiroX &&
+        posicaoInimigoY === posicaoTiroY
+      ) {
+        inimigo.style.display = "none";
+        console.log("Acertou o inimigo!");
+      }
+    }, 10);
+  } catch (err) {
+    console.log(err);
+  }
 
   // Enemy life
 }
